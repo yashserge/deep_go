3eda3f1a5022ca4b7b396f594aa87c3652a0caa4
Yashin Sergey <yashserge@gmail.com>
2025-04-28 23:44:44 +0300

    do homework

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -9,7 +9,15 @@ import (
 // go test -v homework_test.go
 
 func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+	var converted uint32
+
+	converted += 0 +
+		(number&0xFF000000)>>24 +
+		(number&0x00FF0000)>>8 +
+		(number&0x0000FF00)<<8 +
+		(number&0x000000FF)<<24
+
+	return converted
 }
 
 func TestСonversion(t *testing.T) {
