d1524ac6416b786bd2f576c452a9d675195bc825
Yashin Sergey <yashserge@gmail.com>
2025-05-17 16:47:53 +0300

    do homework

--- homework/strings/homework_test.go
+++ homework/strings/homework_test.go
@@ -11,27 +11,74 @@ import (
 type COWBuffer struct {
 	data []byte
 	refs *int
-	// need to implement
 }
 
 func NewCOWBuffer(data []byte) COWBuffer {
-	return COWBuffer{} // need to implement
+	refs := new(int)
+	*refs = 1
+	return COWBuffer{
+		data: data,
+		refs: refs,
+	}
 }
 
 func (b *COWBuffer) Clone() COWBuffer {
-	return COWBuffer{} // need to implement
+	if b == nil {
+		return NewCOWBuffer(nil)
+	}
+
+	*b.refs += 1
+
+	return COWBuffer{
+		data: b.data,
+		refs: b.refs,
+	}
 }
 
 func (b *COWBuffer) Close() {
-	// need to implement
+	if b == nil {
+		return
+	}
+
+	*b.refs -= 1
+	b.refs = nil
+	b.data = nil
 }
 
 func (b *COWBuffer) Update(index int, value byte) bool {
-	return false // need to implement
+	if b == nil {
+		return false
+	}
+
+	if index < 0 || index >= len(b.data) {
+		return false
+	}
+
+	if *b.refs == 1 {
+		b.data[index] = value
+		return true
+	}
+
+	newData := make([]byte, len(b.data))
+	copy(newData, b.data)
+	newData[index] = value
+
+	refs := new(int)
+	*refs = 1
+
+	*b.refs -= 1
+	b.refs = refs
+	b.data = newData
+
+	return true
 }
 
 func (b *COWBuffer) String() string {
-	return "" // need to implement
+	if b == nil {
+		return ""
+	}
+
+	return unsafe.String(unsafe.SliceData(b.data), len(b.data))
 }
 
 func TestCOWBuffer(t *testing.T) {
