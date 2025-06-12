977b2808ca837098e67b223b3f626e565f449080
Yashin Sergey <yashserge@gmail.com>
2025-06-12 21:39:38 +0300

    do homework

--- homework/maps/homewrok_test.go
+++ homework/maps/homewrok_test.go
@@ -10,31 +10,156 @@ import (
 // go test -v homework_test.go
 
 type OrderedMap struct {
-	// need to implement
+	root  *node
+	count int
+}
+
+type node struct {
+	key, value  int
+	left, right *node
+}
+
+// Erase removes a node with provided key from a tree and returns its root.
+func (n *node) Erase(key int) (root *node, erased bool) {
+	var prev, cur *node
+	for cur = n; cur != nil; {
+		if cur.key == key {
+			break
+		} else if cur.key < key {
+			prev = cur
+			cur = cur.right
+		} else {
+			prev = cur
+			cur = cur.left
+		}
+	}
+
+	if cur == nil {
+		return n, false // empty tree or node not found
+	}
+
+	if cur.right == nil {
+		// when right subtree is empty then
+		// just set left subtree instead of current node to parent
+		if prev == nil {
+			return cur.left, true
+		} else if prev.key < key {
+			prev.right = cur.left
+		} else {
+			prev.left = cur.left
+		}
+	} else if cur.left == nil {
+		// when left subtree is empty then
+		// just set right subtree instead of current node to parent
+		if prev == nil {
+			return cur.right, true
+		} else if prev.key < key {
+			prev.right = cur.right
+		} else {
+			prev.left = cur.right
+		}
+	} else {
+		// search for a node to replace with
+		// get the lowest node from right subtree and
+		// remove it and set it instead of current node
+		lowestKey, lowestValue := cur.right.Lowest()
+		cur.right.Erase(*lowestKey)
+		if prev == nil {
+			return &node{key: *lowestKey, value: *lowestValue, left: cur.left, right: cur.right}, true
+		} else if prev.key < key {
+			prev.right = &node{key: *lowestKey, value: *lowestValue, left: cur.left, right: cur.right}
+		} else {
+			prev.left = &node{key: *lowestKey, value: *lowestValue, left: cur.left, right: cur.right}
+		}
+	}
+
+	return n, true
+}
+
+func (n *node) Lowest() (*int, *int) {
+	if n == nil {
+		return nil, nil
+	}
+
+	if n.left == nil {
+		return &n.key, &n.value
+	} else {
+		return n.left.Lowest()
+	}
+}
+
+func (n *node) Contains(key int) bool {
+	if n == nil {
+		return false
+	}
+
+	if n.key == key {
+		return true
+	} else if n.key < key {
+		return n.right.Contains(key)
+	} else {
+		return n.left.Contains(key)
+	}
+}
+
+func (n *node) ForEach(action func(key, value int)) {
+	if n == nil {
+		return
+	}
+
+	n.left.ForEach(action)
+	action(n.key, n.value)
+	n.right.ForEach(action)
 }
 
 func NewOrderedMap() OrderedMap {
-	return OrderedMap{} // need to implement
+	return OrderedMap{}
 }
 
 func (m *OrderedMap) Insert(key, value int) {
-	// need to implement
+	var prev *node
+	for cur := m.root; cur != nil; {
+		if cur.key == key {
+			cur.value = value
+			return
+		} else if cur.key < key {
+			prev = cur
+			cur = cur.right
+		} else {
+			prev = cur
+			cur = cur.left
+		}
+	}
+
+	if prev == nil { // the first value
+		m.root = &node{key: key, value: value}
+	} else if prev.value < value { // new right leaf
+		prev.right = &node{key: key, value: value}
+	} else { // new left leaf
+		prev.left = &node{key: key, value: value}
+	}
+
+	m.count += 1
 }
 
 func (m *OrderedMap) Erase(key int) {
-	// need to implement
+	root, erased := m.root.Erase(key)
+	m.root = root
+	if erased {
+		m.count -= 1
+	}
 }
 
 func (m *OrderedMap) Contains(key int) bool {
-	return false // need to implement
+	return m.root.Contains(key)
 }
 
 func (m *OrderedMap) Size() int {
-	return 0 // need to implement
+	return m.count
 }
 
 func (m *OrderedMap) ForEach(action func(int, int)) {
-	// need to implement
+	m.root.ForEach(action)
 }
 
 func TestCircularQueue(t *testing.T) {
