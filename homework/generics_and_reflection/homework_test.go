44095ebdfa5cec5428de873dd4cd04c1138f1178
Yashin Sergey <yashserge@gmail.com>
2025-06-18 10:06:11 +0300

    do homework

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,8 +19,55 @@ type Person struct {
 }
 
 func Serialize(person Person) string {
-	// need to implement
-	return ""
+	t := reflect.TypeOf(person)
+	v := reflect.ValueOf(person)
+
+	sb := strings.Builder{}
+	for i := 0; i < v.NumField(); i++ {
+		field := t.Field(i)
+
+		var fieldName = field.Name
+		var omitEmpty bool
+
+		opts := strings.Split(field.Tag.Get("properties"), ",")
+		for k := range opts {
+			if opts[k] == "omitempty" {
+				omitEmpty = true
+			} else {
+				fieldName = opts[k]
+			}
+		}
+
+		value := v.Field(i)
+		if omitEmpty && isEmptyValue(value) {
+			continue
+		}
+
+		if i != 0 {
+			sb.WriteString("\n")
+		}
+		sb.WriteString(fmt.Sprintf("%s=%v", fieldName, value.Interface()))
+	}
+
+	return sb.String()
+}
+
+func isEmptyValue(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.String, reflect.Array:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Ptr, reflect.Map, reflect.Slice:
+		return v.IsNil()
+	}
+	return false
 }
 
 func TestSerialization(t *testing.T) {
